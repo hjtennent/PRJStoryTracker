c6260fa2b4938cb78470921eac9c2486555d74b6
Harry Tennent <harry.j.tennent@gmail.com>
2021-03-03 21:22:54 +0000

    Update Android build settings

---
+++ StoryTracker/app/component/styles/History.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  noHistoryContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcomeContainer: {
+    flex: 0.1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 0.5,
+  },
+  storyContainer: {
+    flex: 1,
+  },
+  linkContainer: {
+    borderBottomWidth: 0.5,
+  },
+  linkButton: {
+    margin: 20,
+  },
+  linkText: {
+    fontFamily: 'Oxygen-Regular',
+  }
+});
+
+export default styles;
